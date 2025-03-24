6d6ef9b5bf58dc1eb896e60237fa85ef4992aa69
ramin <raminrezvani@ymail.com>
2025-03-25 01:35:13 +0330

    baseURL correct

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import VueAxios from 'vue-axios'
 axios.defaults.timeout = 1000 * 60 * 3;   // 2 minutes;
 // axios.defaults.baseURL = "https://tour-collector-api.sepehrsmart.ir/";
 // axios.defaults.baseURL = "http://94.74.182.183:8585/";
-axios.defaults.baseURL = "http://127.0.0.1:8585/";
+// axios.defaults.baseURL = "http://127.0.0.1:8585/";
+axios.defaults.baseURL = "http://127.0.0.1:8765/";
 
 const app = createApp(App);
 
